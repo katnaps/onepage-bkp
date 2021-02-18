1f67055ebb93423c16db412415745b622a482d3c
Azri <59246759+katnaps@users.noreply.github.com>
2021-02-18 19:38:00 +0800

    feature section done

--- src/componets/Features.js
+++ src/componets/Features.js
@@ -3,10 +3,12 @@ import { Col, Container, Row } from 'react-bootstrap';
 
 const Features = () => {
     // Icons Row 1
-    const faDesktop = <i class="fas fa-desktop feat-icon"></i>;
-    const faPuzzle = <i class="fas fa-puzzle-piece feat-icon"></i>;
-    const faLifeRing = <i class="far fa-life-ring feat-icon"></i>;
-
+    const faDesktop = <i class="fas fa-desktop fa-xs feat-icon"></i>;
+    const faPuzzle = <i class="fas fa-puzzle-piece fa-xs feat-icon"></i>;
+    const faLifeRing = <i class="far fa-life-ring fa-xs feat-icon"></i>;
+    const faStar = <i class="fas fa-star fa-xs feat-icon"></i>
+    const faCart = <i class="fas fa-shopping-cart fa-xs feat-icon"></i>
+    const faClipboard = <i class="fas fa-clipboard-list fa-xs feat-icon"></i>
 
     const [feat1, setFeat1] = useState([
         {
@@ -27,31 +29,49 @@ const Features = () => {
             title: 'Unlimited Features',
             subtitle: 'Shortcodes, Typography & Different Layouts',
             text: 'Amet sit nulla nostrud non ea proident nulla est quis.',
-            icon: faLifeRing
+            icon: faStar
         },
 
     ]);
 
     const [feat2, setFeat2] = useState([
-        { id: 1, title: 'Clean & Modular Coding', subtitle: '100% Clean, Valid & Well-Commented Coding', text: 'Amet laborum laboris minim nisi.' },
-        { id: 2, title: 'The Best E-Commerce Solutions', subtitle: 'WooCommerce Fully Intergration', text: 'Aute sint deserunt do ut voluptate culpa ea sunt labore duis.' },
-        { id: 3, title: 'Awesome Friendly Support', subtitle: 'Free Lifetime Support & Updates', text: 'Anim esse cillum proident excepteur.' }
+        {
+            id: 1,
+            title: 'Clean & Modular Coding',
+            subtitle: '100% Clean, Valid & Well-Commented Coding',
+            text: 'Amet laborum laboris minim nisi.',
+            icon: faClipboard
+        },
+        {
+            id: 2,
+            title: 'The Best E-Commerce Solutions',
+            subtitle: 'WooCommerce Fully Intergration',
+            text: 'Aute sint deserunt do ut voluptate culpa ea sunt labore duis.',
+            icon: faCart
+        },
+        {
+            id: 3,
+            title: 'Awesome Friendly Support',
+            subtitle: 'Free Lifetime Support & Updates',
+            text: 'Anim esse cillum proident excepteur.',
+            icon: faLifeRing
+        }
     ]);
 
 
 
     return (
         <Container fluid>
-            <Container>
-                <Row className='pt-4'>
+            <Container className='mt-3'>
+                <Row className='pt-5 mb-3'>
 
                     {feat1.map(item => {
                         return (
                             <>
 
                                 <Col className='d-flex justify-content-end col-1'>
-                                        <div className='front-bg'>{item.icon}</div>
-                                        <div className='back-bg'></div>
+                                    <div className='front-bg'>{item.icon}</div>
+                                    <div className='back-bg'></div>
                                 </Col>
 
                                 <Col className='col-3 feat-text'>
@@ -62,8 +82,27 @@ const Features = () => {
                             </>
                         )
                     })}
+                    </Row>
 
-                </Row>
+                    <Row className='mb-5'>
+                        {feat2.map(item => {
+                            return (
+                                <>
+
+                                    <Col className='d-flex justify-content-end col-1'>
+                                        <div className='front-bg'>{item.icon}</div>
+                                        <div className='back-bg'></div>
+                                    </Col>
+
+                                    <Col className='col-3 feat-text'>
+                                        <h4>{item.title}</h4>
+                                        <span>{item.subtitle}</span>
+                                        <p>{item.text}</p>
+                                    </Col>
+                                </>
+                            )
+                        })}
+                    </Row>
             </Container>
         </Container>
     )
